16add77a89f9723b0333bbac997e33d6391462e0
Sayid Abdi <sayid4090@gmail.com>
2022-07-04 18:05:51 -0700

    updated one

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,15 @@
 
 int main(void)
 {
-	char c;
+	int ch;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
 		putchar('\n');
+
+
 		return (0);
 
 }
