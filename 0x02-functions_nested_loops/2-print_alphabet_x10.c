d9579ddd587b55d35f678d492d7532e8942ba8b7
Sayid Abdi <sayid4090@gmail.com>
2022-07-06 14:16:53 -0700

    THe 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ *
+ * print_alphabet_x10 - repeats the print_alphabet 10 times.
+ *
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+		int i;
+
+			char c;
+
+
+
+				for (i = 0; i < 10; ++i)
+
+						{
+
+									for (c = 'a'; c <= 'z'; c++)
+
+												{
+
+																_putchar(c);
+
+																		}
+
+											_putchar('\n');
+
+												}
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
