532d6be90dcabd588d90ef152396e462b9f5432f
Sayid Abdi <sayid4090@gmail.com>
2022-07-08 10:51:39 -0700

    updated onces

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-void positive_or_negative(int i);
-
-int largest_number(int a, int b, int c);
-
-int convert_day(int month,int day);
-
-void print_remaining_days(int month,int day,int year);
-
-void print_to_98(int n);
 
 int _isupper(int c);
 
@@ -31,8 +22,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
-
-void print_number(int n);
-
 
 #endif
