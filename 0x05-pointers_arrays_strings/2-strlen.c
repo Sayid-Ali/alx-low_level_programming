222dff8ca70b4d5fa58c2ca5384af48a6abdc864
Sayid Abdi <sayid4090@gmail.com>
2022-07-12 14:50:51 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ * Return: returns The length ofthe string.
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 
 #define MAIN_H
 int _putchar(char c);
-
 void reset_to_98(int *n);
 swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
