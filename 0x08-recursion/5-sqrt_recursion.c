52d48cfbdec1d58a9237360896676debd2c52633
Sayid Abdi <sayid4090@gmail.com>
2022-07-21 09:35:59 -0700

    Task 5  correct answer Write a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-
-
+int _sqrt(int, int);
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to be used
@@ -9,22 +8,24 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 1 || n == 0)
-		return (n);
-	return (_sqrt(0, n));
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt -> recursive square root
+ * @n: number
+ * @i: iterator
+ * Return: a number
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
 
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- * Return: the square root of n
- */
-int _sqrt(int n, int x)
-{
-	if (n > x / 2)
-		return (-1);
-	else if (n * n == x)
-		return (n);
-	return (_sqrt(n + 1, x));
-}
+
+
