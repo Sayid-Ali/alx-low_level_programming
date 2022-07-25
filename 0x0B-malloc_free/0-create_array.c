22ff7525ef106c61a0ac32d156e2101fda100ac8
Sayid Abdi <sayid4090@gmail.com>
2022-07-25 10:12:10 -0700

    task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create array -> creating array
+ * @size: size of array to be created
+ * @c: characher an array is initialized
+ * Return: a pointer to an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+
+	return (s);
+}
+		
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is about the memory allocation in 0x0B. C - malloc, free
