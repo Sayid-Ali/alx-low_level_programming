96e6b49707f7497b07042541710a89e580397b04
Sayid Abdi <sayid4090@gmail.com>
2022-08-01 10:49:28 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+nclude "dog.h"
+
+#include <stdlib.h>
+
+
+
+/**
+ * free_dog - Free the  dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
